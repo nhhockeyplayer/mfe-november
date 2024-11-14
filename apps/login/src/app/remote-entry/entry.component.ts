64be86aa5cd0d3a397739805552e92f4624cbe0f
nhhockeylayer <meanstack@hotmail.com>
2024-11-14 08:44:57 -0500

    MFE micro front end - Static Module Federation

---
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { inject } from '@angular/core';
+import {UserService} from '@mfe-november/data-access-user'
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  selector: 'mfe-november-login-entry',
+  template: `
+    <div class="login-app">
+      <form class="login-form" (ngSubmit)="login()">
+        <label>
+          Username:
+          <input type="text" name="username" [(ngModel)]="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" [(ngModel)]="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <div *ngIf="isLoggedIn$ | async">User is logged in!</div>
+    </div>
+  `,
+  styles: [
+    `
+      .login-app {
+        width: 30vw;
+        border: 2px dashed black;
+        padding: 8px;
+        margin: 0 auto;
+      }
+      .login-form {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin: 0 auto;
+        padding: 8px;
+      }
+      label {
+        display: block;
+      }
+    `,
+  ],
+})
+export class RemoteEntryComponent {
+  private userService = inject(UserService);
+  username = '';
+  password = '';
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  login() {
+    this.userService.checkCredentials(this.username, this.password);
+  }
+}
